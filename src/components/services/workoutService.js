42b8b4eb6e3f57d40ff3adee50ff125891a5aec8
PJ-Limo <pjslense@gmail.com>
2024-10-14 23:34:00 +0300

    updated workout API component

---
+++ src/components/services/workoutService.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const WGER_API_URL="https://wger.de/api/v2";
+
+export const fetchExercisesByTargetArea = (exercise) => {
+  return axios.get(`${WGER_API_URL}/${exercise}`);
+};
+
+
