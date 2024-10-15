7568e2051aca43e86ec851d9d7e4218287bc82d0
PJ-Limo <pjslense@gmail.com>
2024-10-15 23:06:20 +0300

    amend link color on active

--- src/components/LinkItem.jsx
+++ src/components/LinkItem.jsx
@@ -7,12 +7,8 @@ const LinkItem = ({ href, icon: Icon, text, isSidebarOpen }) => {
     return (
         <li>
             <NavLink
-                className={
-                    `flex gap-4 items-center p-2 text-Navy dark:text-white hover:bg-Navy hover:text-Yellow dark:hover:bg-Yellow
-                    ${isActive
-                        ? 'text-Yellow bg-Navy'
-                        : ''}`
-                }
+                className={`flex gap-4 items-center p-2 text-Navy hover:bg-Navy hover:text-Yellow dark:hover:bg-YellowB
+                    ${isActive ? 'text-Yellow bg-Navy dark:text-Navy dark:bg-Yellow' : 'dark:text-white'}`}
                 to={href}
                 end
             >
