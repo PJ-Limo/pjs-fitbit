06a286685798a0449475c993e7e6dff339f13b37
PJ-Limo <pjslense@gmail.com>
2024-10-20 01:03:14 +0300

    updated error in reps.jsx

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,6 +1,6 @@
 // src/components/StatsPage.js
 import React, { useState, useEffect } from 'react'
-import Reps from './charts/reps'
+import Reps from './charts/Reps'
 import TotalWorkouts from './charts/TotalWorkouts'
 import WeightsLifted from './charts/weightsLifted'
 import CenteredContent from './CenteredContent'
